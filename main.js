820ebb970a10bbd05abfeea1ab7a33edb92c7de9
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-05-18 20:43:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,308 @@
+//14
+// function ages(x=[]){
+// var y=[];
+//     for(var i=0;i<x.length;i++){
+//         if(x[i]>20){
+// y.push(x[i]);
+//         }
+//     }
+
+    
+// console.log(y);
+// }
+
+// ages([2,21,12,3,67,8,99,9,54,4,5,88,9,6,56]);
+
+//asaaf challenge
+// function checklist(num,x=[]){
+
+
+// while(num!=0){
+//     num=prompt("enter item list(to end list enter 0)");
+//     if(num!=0){
+//         x.push(num);
+//         num=confirm("do you want to end list?")
+
+//         if(num==true){
+//             num=0;
+//             num=prompt("enter item list(to end list enter 0)");
+
+//         }
+
+    
+       
+//     }
+// console.log(x);
+//     }
+// }
+
+
+// checklist();
+
+
+//15
+
+// function plusIndex(x=[]){
+
+
+//     for (var i=0;i<x.length;i++){
+// console.log(`${i}${x[i]}`)
+//         // x[i]=`${x[i]}` ;
+//         x[i]=`${i} ${x[i]}`;
+//     }
+
+//     console.log(x);
+// }
+
+// plusIndex(["avi","78","ron"]);
+ //16
+// function even(x=[]){
+// var y=[];
+//     for(var i=0;i<x.length;i++){
+//      if(x[i]%2==0){
+//          x[i]="even"
+//      }   
+//     }
+//     y=x;
+//     return console.log(y);
+// }
+
+// even([1,2,3,4,5,6,7,8]);
+
+//17
+
+// function numbers(num1,num2){
+// num1=parseInt(prompt("enter number 1"));
+// num2=parseInt(prompt("enter number 2"));
+
+// console.log(num1,num2);
+
+
+// }
+
+//18
+// function numbers(num1,num2){
+// num1=parseInt(prompt("enter number 1"));
+// num2=parseInt(prompt("enter number 2"));
+
+// var sum=num1+num2;
+// if(sum==13){
+//     console.log("bar mitzva");
+
+// }
+// else if(sum==12)
+// console.log("bat mitzva");
+// else
+// console.log("kid");
+
+
+
+// }
+
+//19
+
+// function person(age,fname,lname,num){
+//     var i=0;
+// if(age>18){
+// while( i<num){
+//     console.log(fname,lname)
+//     i++;
+// }
+// }
+
+// }
+
+// person(23,"sadsa","levi",46);
+//20
+
+// function printJava(){
+// for(var i=0;i<1000;i++){
+//     console.log("javascript is not java")
+// }
+// }
+
+// printJava();
+
+//21
+
+// function printNum(name,number){
+// name=prompt("enter name");
+// number=parseInt(prompt("enter number"));
+// for(var i=0;i<number;i++){
+//     console.log(name);
+// }
+
+// }
+
+// printNum();
+
+//22
+// function printDetails(name,age,email,number){
+// name=prompt("enter name");
+// age=prompt("enter age");
+// email=prompt("enter email.");
+
+// console.log(name,age,email);
+
+
+// number=parseInt(prompt("enter number"));
+// for(var i=0;i<number;i++){
+
+//     if (age>=18) {
+//         console.log(name,age,email);
+//     }
+// }
+
+// }
+
+// printDetails();
+
+//23
+
+// var number=prompt("enter number");
+
+// for(var i=0;i<number;i++){
+    
+//     var namee=prompt("enter name");
+
+//     if(namee=="znavo"){
+//         console.log("hi bos"+namee)
+//     }
+
+//     else
+//     {
+//         console.log("no entry");
+//     }
+
+// }
+
+//24
+
+var num1=prompt("enter number1");
+var num2=prompt("enter number1");
+
+
+for(var i=0;i<num1;)
+
+
+
+
+
+ 
+//33
+
+
+// function printNumbers(x){
+
+//   x=parseInt(prompt("enter number"));
+
+// var yehidot=x%10;
+// var asrot=x%100/10;
+// var meot=x/100;
+
+// console.log(yehidot);
+// console.log(asrot);
+// console.log(meot);
+
+
+
+
+// }
+
+// printNumbers();
+//יעקב פיתרון שאלה אחרונה 
+// function printNumbers(x){
+
+//     x=parseInt(prompt("enter number"));
+  
+// while(x/10!=0){
+//     var result=x%10;
+//     console.log(result);
+//     x=(x-result)/10;
+
+// }
+  
+//   }
+  
+//   printNumbers();
+
+//30
+
+// function randomEntris(x){
+//     for(var i=0;i<x.length;i++){
+//         if(x[i]*1!=x[i]){
+
+//             x[i]="not_number";
+//         }
+//     }
+
+//     console.log(x);
+// }
+
+// randomEntris([1,2,3,4,"avi","natan",4]);
+
+//35
+
+// function printString(x){
+//     for(var i=0;i<x.length;i++){
+//         console.log(x[i]);
+
+
+//     }
+
+// }
+// printString("hello");
+
+// function returnNewString(x){
+
+//     var newword="";
+//     for(var i=0;i<x.length;i++){
+
+//       console.log(x[i]=x[i]+i);
+
+//       newword+=x[i]+i;
+      
+
+//     }
+
+//    return  console.log(newword);
+    
+
+// }
+// returnNewString("hello");
+
+
+// function changeEvnes(x){
+
+//     var newWord="";
+//   for(var i=0;i<x.length;i++){
+
+// if(i%2==0){
+// newWord+="2";
+// }
+
+// else{
+//     newWord+=x[i];
+// }
+    
+//   }
+
+//   console.log(newWord)
+// }
+
+// changeEvnes("hello")
+
+
+
+
+
+
+
+
+    
+
+
+   
+
+
+
